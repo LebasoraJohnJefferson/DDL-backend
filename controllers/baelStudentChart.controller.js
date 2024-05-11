df69e6e2afa63a4570d1cfa5f53e19cf930aa468
Lebs <johnjefferson.lebasora@evsu.edu.ph>
2024-05-11 09:26:03 +0800

    bael student org CRD

--- controllers/baelStudentChart.controller.js
+++ controllers/baelStudentChart.controller.js
@@ -5,9 +5,61 @@ const {
 
 exports.createStudentChart = async (req,res)=>{
     try {
-        BaelStudentChart.create(req.body)
+        await BaelStudentChart.create(req.body)
         res.status(200).json({message:"Successfully Created!"})
     } catch (error) {
         console.log(error)
     }
 }
+
+
+exports.getStudentOrg = async (req,res)=>{
+    try {
+        const orgDetails = await BaelStudentChart.findAll({
+            include: [
+              {
+                model: User,
+                attributes:{exclude:['password']}
+              },
+            ],
+          })
+
+          const formattedData = orgDetails.map((data)=>{
+            let fullName = `${data?.User?.firstName} ${data?.User?.middleName ? data?.User?.middleName[0] : ''} ${data?.User.lastName} ${data?.User?.suffix ?  data?.User?.suffix : ''}`
+            return {role:data?.role,extension:data?.extension,id:data?.id,fullName:fullName}
+          })
+
+
+          return res.status(200).json({
+            data:formattedData
+          })
+    } catch (error) {
+        
+    }
+}
+
+
+exports.deleteStudentOrgMember = async (req,res)=>{
+    try {
+      const {userId} = req.params
+
+
+      const isExist = await BaelStudentChart.findOne({
+        where:{
+          id:userId
+        }
+      })
+
+      if(!isExist) return res.status(404).json({message:'Faculty Member not Found!'})
+
+      await isExist.destroy()
+
+      return res.status(204).json({
+        message:'Successfully deleted'
+      })
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
--- routes/baelStudentChart.js
+++ routes/baelStudentChart.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const {verifyToken} = require("../middlewares/generateToken")
 const {
-    createStudentChart
+    createStudentChart,
+    getStudentOrg,
+    deleteStudentOrgMember
 } = require("../controllers/baelStudentChart.controller");
 const {isAdmin} = require("../middlewares/checkRole")
 
 
 router.post("/", verifyToken,isAdmin,createStudentChart);
+router.delete("/:userId", verifyToken,isAdmin,deleteStudentOrgMember);
+router.get("/", verifyToken,isAdmin,getStudentOrg);
 
 module.exports = router;
