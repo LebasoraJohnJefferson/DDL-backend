79c04072f7e2d72f38037842bab521f416e71aea
Lebs <johnjefferson.lebasora@evsu.edu.ph>
2024-05-29 23:03:14 +0800

    feat: shared file admin

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,5 +1,6 @@
 const {
     User,
+    File
   } = require("../models");
 
 exports.getAdmin = async (req, res) => {
@@ -22,3 +23,50 @@ exports.getAdmin = async (req, res) => {
     handleError(error, res);
   }
 };
+
+
+exports.getAllOwner = async(req,res)=>{
+  try {
+    const owners = await File.findAll({
+      include:[{
+        model:User,
+        attributes:{exclude:['password']},
+      }],
+      group:['User.id']
+    })
+
+
+    return res.status(200).json({users:owners})
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+exports.getAllFilesByOwnerId = async(req,res)=>{
+  try {
+    const {ownerId} = req.params;
+
+    const isOwnerExist = await User.findOne({
+      where:{
+        id:ownerId
+      }
+    })
+
+    if(!isOwnerExist) return res.status(404).json({message:"Owner doens`t Exist!"})
+
+
+    const files = await File.findAll({
+      where:{
+        userId:ownerId
+      },
+      order: [['createdAt', 'DESC']]
+    })
+
+    res.status(200).json({owner:isOwnerExist,files:files})
+  } catch (error) {
+    console.log(error)
+  }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 const {
-    getAdmin
+    getAdmin,
+    getAllOwner,
+    getAllFilesByOwnerId
 } = require("../controllers/admin.controller")
 
 
 router.get("/" ,getAdmin);
+router.get("/files/owners",getAllOwner)
+router.get("/files/owners/:ownerId",getAllFilesByOwnerId)
 
 
 module.exports = router;
